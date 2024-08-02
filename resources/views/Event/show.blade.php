03aabbda947a889a5f446353823343170d2c564b
vanzvb <vanzbelleza70@gmail.com>
2024-08-02 23:53:51 +0800

    Cleaned Event Show Section

--- resources/views/Event/show.blade.php
+++ resources/views/Event/show.blade.php
@@ -1,4 +1,8 @@
-@extends('layouts.app')
+@extends('layouts.adminlte')
+
+@php
+    use Carbon\Carbon;
+@endphp
 
 @section('content')
     <div class="row">
@@ -12,53 +16,64 @@
         </div>
     </div>
 
-    <div class="row">
-        <div class="col-xs-12 col-sm-12 col-md-12">
+    <div class="row mt-4 text-center">
+        <div class="col-md-12">
             <div class="form-group">
-                <strong>Name:</strong>
-                {{ $event->name }}
+                <h3><strong>Name:</strong> {{ $event->name }}</h3>
             </div>
             <div class="form-group">
-                <strong>Details:</strong>
-                {{ $event->detail }}
+                <p><strong>Details:</strong> {{ $event->detail }}</p>
             </div>
             <div class="form-group">
-                <strong>Event ID:</strong>
-                {{ $event->id }}
+                <p><strong>Start Date:</strong> {{ Carbon::parse($event->start_date)->format('F j, Y') }}</p>
+            </div>
+            <div class="form-group">
+                <p><strong>End Date:</strong> {{ Carbon::parse($event->end_date)->format('F j, Y') }}</p>
+            </div>
+            <div class="form-group">
+                <p><strong>Event ID:</strong> {{ $event->id }}</p>
             </div>
         </div>
     </div>
 
-    <div class="row">
-        <div class="col-xs-12 col-sm-12 col-md-12">
-            <h3>Event Details</h3>
+    <div class="row mt-4">
+        <div class="col-md-12">
+            <h3 class="text-center">Event Details</h3>
             @if(isset($eventDetails) && $eventDetails->isNotEmpty())
-                @foreach($eventDetails as $eventDetail)
-                    <div class="form-group">
-                        <strong>Event Detail ID:</strong>
-                        {{ $eventDetail->id }}
+                @foreach($eventDetails->chunk(4) as $chunk)
+                    <div class="row">
+                        @foreach($chunk as $eventDetail)
+                            <div class="col-lg-3 col-md-4 mb-4">
+                                <div class="card shadow-sm">
+                                    <div class="card-body">
+                                        <h5 class="card-title">Event Detail ID: {{ $eventDetail->id }}</h5>
+                                        <p class="card-text">
+                                            <strong>User ID:</strong> {{ $eventDetail->userID }}
+                                        </p>
+                                        <!-- Display user information if needed -->
+                                        @if($users->find($eventDetail->userID))
+                                            @php
+                                                $user = $users->find($eventDetail->userID);
+                                            @endphp
+                                            <p class="card-text">
+                                                <strong>Full Name:</strong> {{ $user->getFullNameAttribute() }}
+                                            </p>
+                                            <p class="card-text">
+                                                <strong>User Email:</strong> {{ $user->email }}
+                                            </p>
+                                        @endif
+                                        <!-- Placeholder Button -->
+                                        <a href="#" class="btn btn-primary">Placeholder Button</a>
+                                    </div>
+                                </div>
+                            </div>
+                        @endforeach
                     </div>
-                    <div class="form-group">
-                        <strong>User ID:</strong>
-                        {{ $eventDetail->userID }}
-                    </div>
-                    <!-- Display user information if needed -->
-                    @if($users->find($eventDetail->userID))
-                        @php
-                            $user = $users->find($eventDetail->userID);
-                        @endphp
-                        <div class="form-group">
-                            <strong>User Name:</strong>
-                            {{ $user->getFullNameAttribute() }}
-                        </div>
-                        <div class="form-group">
-                            <strong>User Email:</strong>
-                            {{ $user->email }}
-                        </div>
-                    @endif
                 @endforeach
             @else
-                <p>No event details found.</p>
+                <div class="alert alert-info text-center" role="alert">
+                    No event details found.
+                </div>
             @endif
         </div>
     </div>
